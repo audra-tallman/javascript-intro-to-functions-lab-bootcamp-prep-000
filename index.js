6e46600b011ba76940d71be2462b07981572d020
Audra Tallman <audra.tallman@hotmail.com>
2019-02-07 21:06:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase 
+
+function sayHiToGrandma(string) {
+  return string("I cant hear you!")
+  if === 
+}
