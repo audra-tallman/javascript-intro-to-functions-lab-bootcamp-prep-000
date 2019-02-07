1e69840e4f8c585a167c52ea3be5e5e7d190a4eb
Audra Tallman <audra.tallman@hotmail.com>
2019-02-07 22:30:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 function logShout(string) {
  console.log(string.toUpperCase());
@@ -12,18 +12,18 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) { 
-  var lowercase = "hello!" 
+  var lowercase = "hello!"; 
   lowercase.toLowerCase() === lowercase;
   return ("I can't hear you");
   } 
   else if (string === string.toUpperCase()) {
-  var uppercase = "HELLO!" 
-  uppercase.toUpperCase() === uppercase 
+  var uppercase = "HELLO!"; 
+  uppercase.toUpperCase() === uppercase; 
   return("YES INDEED!");
-  } else if (string='mixedCase') {
-  var mixedCase =  "I love you, Grandma."
-  mixedCase.toLowerCase() === mixedCase
-  mixedCase.toUpperCase() === mixedCase
+  } else if (string === 'mixedCase') {
+  var mixedCase =  "I love you, Grandma.";
+  mixedCase.toLowerCase() === mixedCase;
+  mixedCase.toUpperCase() === mixedCase;
   return string("I love you, too.");
   }
   }
